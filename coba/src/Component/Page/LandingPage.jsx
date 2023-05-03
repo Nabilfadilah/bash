086e1568a67f8eb0c022e684a5a678e1968a1ae5
Nabilfadilah <mohammadfadilah00@gmail.com>
2023-05-03 14:25:29 +0700

    Membuat halaman landing page

---
+++ coba/src/Component/Page/LandingPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function LandingPage() {
+    return (
+        <div className='landingpage'>
+            <h3>Landing page</h3>
+        </div>
+    )
+}
+
+export default LandingPage
